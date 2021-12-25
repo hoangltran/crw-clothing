a0bd66b36ca4a8b95c6783f137791ac5ff403c17
Hoang Tran <tranluuhoang@gmail.com>
2021-12-25 12:38:01 -0700

    Rename cart-dropdown component, user reselect to enable memoization for cartitems and cartitemscount'

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,7 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assests/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.styles";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({ currentUser, hidden }) => (
   <div className="header">
